2c1889349e64397cb77caa036efe84b4821d5749
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-04-10 11:33:17 -0500

    done with card maker function

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -19,10 +19,34 @@
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
 
-axios.get('https://lambda-times-backend.herokuapp.com/articles')
-.then(response => {
-    console.log(response)
-})
-.catch(err => {
-    console.log(err)
-})
+
+
+function cardMaker(user){
+    const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const author = document.createElement('div')
+    const imgContainer = document.createElement('div')
+    const img = document.createElement('img')
+    const authorName = document.createElement('span')
+
+    card.appendChild(headline)
+    card.appendChild(author)
+    author.appendChild(imgContainer)
+    imgContainer.appendChild(img)
+    author.appendChild(authorName)
+
+    card.classList.add('card')
+    headline.classList.add('headline')
+    author.classList.add('author')
+    imgContainer.classList.add('img-container')
+
+    img.src = user.authorPhoto
+
+    headline.textContent = user.headline
+    authorName.textContent = user.authorName
+
+    return card
+
+}
+    const cardsContainer = document.querySelector('.cards-container') 
+
